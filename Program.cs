83b533fef3e5b720edad05c58337739ea511021f
Laetitia <laeptitchat29@gmail.com>
2026-05-18 11:06:28 +0200

    readLine

--- Program.cs
+++ Program.cs
@@ -1,14 +1,35 @@
-﻿string name = "Laetitia";
+﻿using System.Data;
+
+string name = "Laetitia";
 char letter = 'A';
 int number = 42;
 float pi = 3.14159254893626f;
+float temperature = 25.5f;
 double e = 2.71828555445555888889966333333333321;
 decimal money = 19.99m;
+bool isRaining = true;
+var message = "Welcome to C# programming!";
 Console.WriteLine("Hello, " + name + "!");
 Console.WriteLine(number);
 Console.WriteLine(letter);
 Console.WriteLine(pi);
 Console.WriteLine(Math.Round(e,2));
 Console.WriteLine(money);
-//Exercice.Addition(5, 3);
+Console.WriteLine(isRaining);
+
+Exercice.Addition(5, 3);
 Exercice.Substraction(10, 4);
+
+money += 10;
+Console.WriteLine(money);
+Console.WriteLine(message);
+Console.Write("Hello, " + name + "! ");
+Console.Write("You have " + number + " messages in your inbox. ");
+Console.WriteLine("The temperature is " + temperature + " degrees Celsius.");
+
+Console.WriteLine("Entrez votre prénom : ");
+string prenom = Console.ReadLine();
+Console.WriteLine("Bonjour, " + prenom + "!");
+
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
